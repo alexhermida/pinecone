4c447b3bc13f329e1cb32b953e2a1f54e5f99a19
Álex Hermida <alexhermida@gmail.com>
2018-04-09 22:26:38 +0200

    Add README and version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vigotech.org 
+
+## Pinecone managament site
+
+*Bootstrapping..*
---
+++ backend/conf/__about__.py
@@ -0,0 +1,8 @@
+__title__ = 'pinecone-backend'
+__summary__ = 'Domain.'
+__version__ = '0.0.1-dev'
+
+__license__ = 'All rights reserved.'
+__uri__ = 'http://vigotech.org/'
+__author__ = 'VigoTech'
+__email__ = 'alliance@vigotech.org'
