7bbc92c3a1e3b0b7e618f039721beb5b6e99b50c
Álex Hermida <alexhermida@gmail.com>
2018-05-01 17:20:45 +0200

    Updated package & tests

--- backend/core/tests/test_functional.py
+++ backend/core/tests/test_functional.py
@@ -5,3 +5,14 @@ import pytest
 def test_admin(client):
     response = client.get('/admin/login/')
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_unauthenticated(client):
+    response = client.get('/api/')
+    assert response.status_code == 401
+
+
+def test_authenticated(admin_client):
+    response = admin_client.get('/api/')
+    assert response.status_code == 200
