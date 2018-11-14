5a35b8c1e4b5082da183a650ae7ea62176d02a58
Álex Hermida <alexhermida@gmail.com>
2018-11-14 23:13:46 +0100

    Fix PEP8 functional test

--- backend/tests/test_functional.py
+++ backend/tests/test_functional.py
@@ -165,7 +165,8 @@ def test_update_published_event(admin_client):
 
     assert response.status_code == 200
     assert response.json()['google_calendar_published'] is True
-    assert response.json()['start'] == new_start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+    assert response.json()['start'] == \
+           new_start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 
 def test_prevent_create_event_without_description(admin_client):
